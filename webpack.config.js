84d16308f96284953874d607cc020193e8f88a10
sunkuangdong <sunkuangdong@ele-cloud.com>
2021-01-12 15:43:23 +0800

    报错了

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,12 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require("path")
 
 module.exports = {
     mode: "production",
-    entry: "./src/main.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [{
-                test: /\.css$/,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../',
-                        },
-                    },
-                    'css-loader',
-                ]
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: [
-                    "style-loader",
-                    'css-loader',
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("dart-sass")
-                        }
-                    },
-                ]
-            },
-            {
-                test: /\.ts$/,
-                use: "ts-loader"
-            }
-        ]
-    },
+    entry: "./src/index.js",
     plugins: [
         new HtmlWebpackPlugin({
             template: "./index.html"
         }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: '[id].css',
-        })
     ],
 }
